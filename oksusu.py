8d3e664de1941b274d74e09bb82c3c53c1993890
soju6jan <cybersol@naver.com>
2018-08-02 04:51:18 +0900

    update

--- oksusu.py
+++ oksusu.py
@@ -170,7 +170,7 @@ def GetList( type, param, page ):
 			request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
 			response = urllib2.urlopen(request)
 			data = json.load(response, encoding="utf-8")
-			LOG(data)
+			#LOG(data)
 			for item in data["channels"]:
 				try:
 					info = {}
